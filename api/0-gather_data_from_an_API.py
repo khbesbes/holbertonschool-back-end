4e807605eae304d19f13601d5612a90aba999c0c
khbesbes <5786@holbertonstudents.com>
2023-08-22 17:08:09 +0100

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-'''returns information about his/her TODO list progress.'''
+"""Script to use a REST API for a given employee ID, returns
+information about his/her TODO list progress"""
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com'
-    user_id = argv[1]
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"UsageError: python3 {__file__} employee_id(int)")
+        sys.exit(1)
+
+    API_URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
+
     response = requests.get(
-        f'{url}/users/{user_id}/todos',
-        params={'_expand': 'user'}
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
     )
+    data = response.json()
 
-    if response.status_code == 200:
-        data = response.json()
-        name = data[0]['user']['name']
-        tasks_ok = [task for task in data if task['completed']]
-        n_task_ok = len(tasks_ok)
-        total_task = len(data)
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
 
-        first_str = f"Employee {name} is done with tasks"
+    employee_name = data[0]["user"]["name"]
+    total_tasks = len(data)
+    done_tasks = [task for task in data if task["completed"]]
+    total_done_tasks = len(done_tasks)
 
-        print(f"{first_str}({n_task_ok}/{total_task}):")
-        for task in tasks_ok:
-            print(f"\t {task['title']}")
-
-    else:
-        print(f"Error: {response.status_code}")
+    print(f"Employee {employee_name} is done with tasks"
+          f"({total_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task['title']}")
